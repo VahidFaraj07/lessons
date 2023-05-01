89b9d395f46a59082c066261167ba09bb5b09518
Vahid Farajzadeh <vahid.faraj@hotmail.com>
2023-05-01 21:07:45 +0400

    if-else && switch-case

---
+++ Aprel/30/Homework1/Homework1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/*
+ Homework 1:
+ Əvvəlcədən iki dəyişəndə username və password saxlayın. Bunlar heç bir zaman dəyişməyəcək şəkildə olsun. Yəni proqramda belə dəyişdirmək mümkün olmasın.
+ Sonra istifadəçidən username və password'u daxil etməsini istəyin. Əgər istifadəçi doğru məlumatları yazsa ekrana "Sisteme daxil olundu." mesajı çıxsın.
+ Yox əgər səhv məlumat daxil etsə, yenidən username və password'u daxil etməsini istəyin. 3 dəfə səhv daxil etdikdən sonra "Hesab bloklandi" yazsın.
+ */
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string usernameDB = "vahid.faraj";
+            const string passwordDB = "Admin123";
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if (usernameDB == username && passwordDB == password)
+            {
+                Console.WriteLine("Sisteme daxil olundu.");
+            }
+            else
+            {
+                Console.WriteLine("Username ve Password'u yeniden daxil edin.");
+
+                Console.Write("Username: ");
+                username = Console.ReadLine();
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+                if (usernameDB == username && passwordDB == password)
+                {
+                    Console.WriteLine("Sisteme daxil olundu.");
+                }
+                else
+                {
+                    Console.WriteLine("Username ve Password'u yeniden daxil edin.");
+                    Console.Write("Username: ");
+                    username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+                    if (usernameDB == username && passwordDB == password)
+                    {
+                        Console.WriteLine("Sisteme daxil olundu.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hesab bloklandi");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Aprel/30/Homework2/Homework2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+/*
+Homework 2:
+Switch-case ilə sadə kalkulyator app'ı yazacaqsız. İlk öncə istifadəçiyə kiçik menyu çıxardın. Orda edə biləcəyi əməliyyatlar olacaq. Toplama, çıxma, vurma və bölmə. 
+İstifadəçi hansını seçsə həmin case'də ekrana nəticəni çıxardacaqsız. Nəticəni göstərəndən sonra aşağıda yenidən ana menyunu göstərəcəksiz. 
+İstifadəçi istədiyi qədər yəni proqramı bağlayana qədər bu döngü halında davam edəcək.
+ 
+*/
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int action = 0, a = 0, b = 0;
+            switch (action)
+            {
+                case 0:
+                    Console.Write("Asagidaki emeliyyatlardan birini secin.\n1. Toplama\n2. Cixma\n3. Vurma\n4. Bolme\nSeciminiz: ");
+                    action = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("a = ");
+                    a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("b = ");
+                    b = Convert.ToInt32(Console.ReadLine());
+
+                    if (action == 1)
+                    {
+                        goto case 1;
+                    }
+                    else if (action == 2)
+                    {
+                        goto case 2;
+                    }
+                    else if (action == 3)
+                    {
+                        goto case 3;
+                    }
+                    else if (action == 4)
+                    {
+                        goto case 4;
+                    }
+                    else {
+                        break;
+                    }
+
+                case 1:
+                    Console.WriteLine($"Cavab: {a + b}\n");
+                    goto case 0;
+                case 2:
+                    Console.WriteLine($"Cavab: {a - b}\n");
+                    goto case 0;
+                case 3:
+                    Console.WriteLine($"Cavab: {a * b}\n");
+                    goto case 0;
+                case 4:
+                    Console.WriteLine($"Cavab: {(double)a / (double)b}\n");
+                    goto case 0;
+
+            }
+        }
+    }
+}
