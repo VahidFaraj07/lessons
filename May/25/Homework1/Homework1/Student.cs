39908c5bd21157b6c4a058e04dd6a5081916f0df
Vahid Farajzadeh <vahid.faraj@hotmail.com>
2023-05-27 17:08:20 +0400

    properties

---
+++ May/25/Homework1/Homework1/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Student
+    {
+        private string name, surname, email = null;
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+
+                if (surname != null)
+                {
+                    email = $"{name}.{surname}@gmail.com";
+                }
+            }
+
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Surname
+        {
+            set
+            {
+                surname = value;
+                if (name != null)
+                {
+                    email = $"{name}.{surname}@gmail.com";
+                }
+            }
+
+            get
+            {
+                return surname;
+            }
+        }
+    }
+}
