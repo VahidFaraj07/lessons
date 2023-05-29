5508ad79ffbf81e5b8a4773f4509f8d0de43ae5d
Vahid Farajzadeh <vahid.faraj@hotmail.com>
2023-05-30 00:07:00 +0400

    static, setter, getter, instance

---
+++ May/28/Homework/Homework/Customer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Homework
+{
+    public class Customer
+    {
+        private string username, password;
+        private static ArrayList db;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+
+        public string Username
+
+        {
+            get
+            {
+                return this.username;
+            }
+            set
+            {
+                if (isUsernameExists(value))
+                {
+                    this.username = null;
+                }
+                else
+                {
+                    this.username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (passIsOkay(value))
+                {
+                    this.password = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Sifre en az 8 simvoldan ibaret, icinde en az 1 boyuk herf ve 1 reqem olmalidir.");
+                }
+            }
+        }
+
+
+        static Customer()
+        {
+            db = new ArrayList();
+        }
+
+        public static void AddCustomer(Customer obj)
+        {
+            if (!isNullOrEmpty(obj.Id.ToString(), obj.Name, obj.Surname, obj.Email, obj.username, obj.password))
+            {
+                if (isEmailExists(obj.Email))
+                {
+                    Console.WriteLine("Elave etmek istediyiniz email artiq sistemde movcuddur!");
+                }
+                else
+                {
+                    db.Add(obj);
+                    Console.WriteLine("Yeni musteri sisteme elave olundu!");
+                }
+            }
+        }
+
+        private static bool isUsernameExists(string tempUsername)
+        {
+            foreach (Customer item in db)
+            {
+                if (item.username.ToString() == tempUsername)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool passIsOkay(string tempPass)
+        {
+            bool hasLower = false;
+            bool hasUpper = false;
+            bool hasDigit = false;
+            if (tempPass.Length < 8)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (char c in tempPass)
+                {
+                    if (Char.IsLower(c))
+                    {
+                        hasLower = true;
+                    }
+                    else if (Char.IsUpper(c))
+                    {
+                        hasUpper = true;
+                    }
+                    else if (Char.IsDigit(c))
+                    {
+                        hasDigit = true;
+                    }
+                    if (hasLower && hasUpper && hasDigit)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool isEmailExists(string tempEmail)
+        {
+            foreach (Customer item in db)
+            {
+                if (item.Email == tempEmail)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private static bool isNullOrEmpty(params string[] par)
+        {
+            foreach (string item in par)
+            {
+                if (item == null || item == "")
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
+}
