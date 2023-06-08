8e4fcd6af87d19e7e76a2a895947b467de9adc0a
Vahid Farajzadeh <vahid.faraj@hotmail.com>
2023-06-09 00:19:51 +0400

    OOP - Inheritance

--- Iyun/04/Homework2/Homework2/Base.cs
+++ Iyun/04/Homework2/Homework2/Base.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework2
 {
@@ -10,6 +6,6 @@ namespace Homework2
     {
         public int Id { get; set; }
         public DateTime CreateDate { get; set; }
-        public DateTime EditDate { get; set; }
+        public DateTime? EditDate { get; set; }
     }
 }
