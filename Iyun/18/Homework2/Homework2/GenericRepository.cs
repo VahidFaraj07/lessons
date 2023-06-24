97292a71138c7bae1f52a46f917e3c1c0249198c
Vahid Fereczade <vahid.faraj@hotmail.com>
2023-06-24 17:42:18 +0400

    Generic Repository

--- Iyun/18/Homework2/Homework2/GenericRepository.cs
+++ Iyun/18/Homework2/Homework2/GenericRepository.cs
@@ -14,35 +14,48 @@ namespace Homework2
             items = new List<T>();
         }
 
+        #region Add
         public void Add(T obj)
         {
             items.Add(obj);
         }
+        #endregion
+
 
         public void Update(T obj, int id)
         {
+
         }
 
+        #region Get
         public void Get(int id)
         {
-            foreach (var obj in items)
+            foreach (T obj in items)
             {
                 Type type = obj.GetType();
                 PropertyInfo[] properties = type.GetProperties();
-
                 foreach (PropertyInfo property in properties)
                 {
-                    Console.WriteLine($"{property.Name}: {property.GetValue(obj)}");
+                    if (property.Name == "Id" && (int)property.GetValue(obj) == id)
+                    {
+                        ShowAllProperties(obj, properties);
+                    }
                 }
-                Console.WriteLine();
-            }                   
+            }
         }
+
+        private void ShowAllProperties(T obj, PropertyInfo[] properties)
+        {
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.Name}: {property.GetValue(obj)}");
+            }
+        }
+        #endregion
 
         public void Delete(int id)
         {
             items.RemoveAt(id);
         }
-
-
     }
 }
