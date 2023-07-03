22e0c77b0d6c0a2d66c0584dc996775682d605c0
Vahid Fereczade <vahid.faraj@hotmail.com>
2023-07-03 17:03:14 +0400

    Winform - loginpage, write data to virtual database

---
+++ Iyun/29/WindowsFormsPart2/WindowsFormsPart2/HomePage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPart2
+{
+    public partial class HomePage : Form
+    {
+        public HomePage(User user)
+        {
+            InitializeComponent();
+
+            nameEdit.Text = user.Name;
+            surnameEdit.Text = user.Surname;
+            usernameEdit.Text = user.Username;
+            passwordEdit.Text = user.Password;
+            aboutUserEdit.Text = user.AboutUser;
+            saveUserInfoBtn.Tag = user.Id;
+        }
+
+        private void saveUserInfoBtn_Click(object sender, EventArgs e)
+        {
+            int id = (int)saveUserInfoBtn.Tag;
+            int index = VirtualDatabase.Users.FindIndex(i => i.Id == id);
+            bool passwordIsChanged = false;
+
+            if (VirtualDatabase.Users[index].Password != passwordEdit.Text)
+            {
+                passwordIsChanged = true;
+            }
+
+            VirtualDatabase.Users[index].Name = nameEdit.Text;
+            VirtualDatabase.Users[index].Surname = surnameEdit.Text;
+            VirtualDatabase.Users[index].Username = usernameEdit.Text;
+            VirtualDatabase.Users[index].Password = passwordEdit.Text;
+            VirtualDatabase.Users[index].AboutUser = aboutUserEdit.Text;
+
+            MessageBox.Show("Melumatlar deyisdirildi!");
+
+            if (passwordIsChanged)
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Iyun/29/WindowsFormsPart2/WindowsFormsPart2/VirtualDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsPart2
+{
+    public class VirtualDatabase
+    {
+        public static List<User> Users = new List<User>();
+
+        static VirtualDatabase()
+        {
+            Users.Add(new User()
+            {
+                Id = 1,
+                Name = "vahid",
+                Surname = "faraj",
+                Username = "v",
+                Password = "f"
+            });
+
+            Users.Add(new User()
+            {
+                Id = 2,
+                Name = "silver",
+                Surname = "light",
+                Username = "s",
+                Password = "l"
+            });
+        }
+    }
+}
