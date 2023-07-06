6f8c14fc34ca7d082334eef353b8eb8944ebdb39
Vahid Fereczade <vahid.faraj@hotmail.com>
2023-07-06 11:02:24 +0400

    tic tac toe

---
+++ Iyul/05/MyFirstGame/MyFirstGame/MainPage.Designer.cs
@@ -0,0 +1,233 @@
+﻿using System.Drawing;
+
+namespace MyFirstGame
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.White;
+            this.button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button.FlatAppearance.BorderSize = 0;
+            this.button.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button.ForeColor = System.Drawing.Color.Black;
+            this.button.Location = new System.Drawing.Point(9, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(100, 100);
+            this.button.TabIndex = 0;
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(115, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(221, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(9, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(115, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(221, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(9, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(115, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.button);
+            this.groupBox.Controls.Add(this.button3);
+            this.groupBox.Controls.Add(this.button5);
+            this.groupBox.Controls.Add(this.button6);
+            this.groupBox.Controls.Add(this.button2);
+            this.groupBox.Controls.Add(this.button1);
+            this.groupBox.Controls.Add(this.button8);
+            this.groupBox.Controls.Add(this.button7);
+            this.groupBox.Controls.Add(this.button4);
+            this.groupBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(330, 330);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.Font = new System.Drawing.Font("Ebrima", 50F);
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(221, 224);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 381);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(330, 45);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Reset";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(354, 457);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.groupBox);
+            this.Font = new System.Drawing.Font("Ebrima", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainPage";
+            this.Text = "Xgame";
+            this.groupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Iyul/05/MyFirstGame/MyFirstGame/MainPage.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstGame
+{
+    public partial class MainPage : Form
+    {
+        public static string whoIsNext = "O";
+        public static List<string> charList = new List<string>(9);
+
+        static MainPage()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                charList.Add("");
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (whoIsNext == "O")
+            {
+                btn.Text = "O";
+                btn.BackColor = Color.Red;
+                charList.Insert(btn.TabIndex, "O");
+                whoIsNext = "X";
+            }
+            else if (whoIsNext == "X")
+            {
+                btn.Text = "X";
+                btn.BackColor = Color.Green;
+                charList.Insert(btn.TabIndex, "X");
+                whoIsNext = "O";
+            }
+
+            btn.Enabled = false;
+            CheckGameStatus(btn.Text);
+
+        }
+
+        private void CheckGameStatus(string gamer)
+        {
+            bool gamerIsWin = false;
+            if (charList[0] == gamer && charList[1] == gamer && charList[2] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[3] == gamer && charList[4] == gamer && charList[5] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[6] == gamer && charList[7] == gamer && charList[8] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[0] == gamer && charList[3] == gamer && charList[6] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[1] == gamer && charList[4] == gamer && charList[7] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[2] == gamer && charList[5] == gamer && charList[8] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[0] == gamer && charList[4] == gamer && charList[8] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else if (charList[2] == gamer && charList[4] == gamer && charList[6] == gamer)
+            {
+                gamerIsWin = true;
+            }
+            else
+            {
+                if (allButtonIsSelected())
+                {
+                    MessageBox.Show("Oyun bitmisdir yeniden baslayin!");
+                }
+            }
+
+            if (gamerIsWin)
+            {
+                MessageBox.Show($"{gamer} oyuncu qalibdir!!");
+            }
+        }
+
+        private bool allButtonIsSelected()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (charList[i] == "")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            charList.Clear();
+
+            foreach (var item in groupBox.Controls)
+            {
+                if (item is Button)
+                {
+                    Button tempButton = (Button)item;
+                    tempButton.Text = "";
+                    tempButton.Enabled = true;
+                    tempButton.BackColor = Color.White;
+                    for (int i = 0; i < 8; i++)
+                    {
+                        charList.Add("");
+                    }
+                }
+            }
+        }
+    }
+}
